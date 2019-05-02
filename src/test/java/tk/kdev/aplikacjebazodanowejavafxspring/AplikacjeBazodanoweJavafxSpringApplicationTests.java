89682d20c28475b2c122c41d578b7dcb6cb76a2e
Krystian Lukasiak <kryslukasiak@gmail.com>
2019-05-02 23:34:39 +0200

    initialize project

---
+++ src/test/java/tk/kdev/aplikacjebazodanowejavafxspring/AplikacjeBazodanoweJavafxSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package tk.kdev.aplikacjebazodanowejavafxspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AplikacjeBazodanoweJavafxSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
